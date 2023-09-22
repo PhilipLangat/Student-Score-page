e6845dfa5bb289a70d8faaa1f919cefbb4ba7c5b
Philip langat <111146779+PhilipLangat@users.noreply.github.com>
2023-09-22 12:01:36 +0300

    Add files via upload
    The Javascript code.

---
+++ script.js
@@ -0,0 +1,8 @@
+        document.addEventListener('DOMContentLoaded', function () {
+            const continueButton = document.getElementById('continueButton');
+
+            continueButton.addEventListener('click', function () {
+                alert('You clicked the "Continue" button!');
+
+                    });
+});
